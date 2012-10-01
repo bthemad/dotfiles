2d70c5cbfed47c3d274829ab52831e605ff30e1b
Alex Kudryashov <alex.kudryashov@gmail.com>
2012-10-01 15:24:15 +0200

    Adding new files to init script

--- init.sh
+++ init.sh
@@ -4,9 +4,12 @@ rm ./.bashrc
 rm ./.gitconfig
 rm ./.tmux
 rm ./.zshrc
+rm ./.gdbinit
+rm ./.cgdbrc
 
 ln -s ~/.dotfiles/bash_profile ./.bash_profile
 ln -s ~/.dotfiles/bashrc ./.bashrc
 ln -s ~/.dotfiles/gitconfig ./.gitconfig
 ln -s ~/.dotfiles/tmux.conf ./.tmux.conf
 ln -s ~/.dotfiles/zshrc ./.zshrc
+ln -s ~/.dotfiles/cgdbrc ./.cgdbrc
