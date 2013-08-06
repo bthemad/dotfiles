70dfa77e381979fd52a96979330cf7aeedcdb929
Alex Kudryashov <alex.kudryashov@gmail.com>
2013-08-06 03:19:36 +0200

    Added boilerplate emacs config and modified path to include zsh completion functions

---
+++ init.el
@@ -0,0 +1,100 @@
+(setq mac-command-modifier 'meta)
+
+;; Requisites: Emacs >= 24
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+(package-refresh-contents)
+
+(defun install-if-needed (package)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+;; make more packages available with the package installer
+(setq to-install
+      '(python-mode magit yasnippet jedi auto-complete autopair 
+                    flymake-python-pyflakes exec-path-from-shell 
+                    better-defaults ido-hacks smex))
+
+(mapc 'install-if-needed to-install)
+
+(require 'magit)
+(global-set-key "\C-xg" 'magit-status)
+
+(require 'auto-complete)
+(require 'autopair)
+(require 'flymake)
+(require 'yasnippet)
+
+; auto-complete mode extra settings
+(setq
+ ac-auto-start 2
+ ac-override-local-map nil
+ ac-use-menu-map t
+ ac-candidate-limit 20)
+
+;; ;; Python mode settings
+(require 'python-mode)
+(add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
+(setq py-electric-colon-active t)
+(add-hook 'python-mode-hook 'autopair-mode)
+(add-hook 'python-mode-hook 'yas-minor-mode)
+
+;; ;; Jedi settings
+(require 'jedi)
+;; if you need to change your python intepreter, if you want to change it
+;; (setq jedi:server-command
+;;       '("python2" "/home/andrea/.emacs.d/elpa/jedi-0.1.2/jediepcserver.py"))
+
+(add-hook 'python-mode-hook
+	  (lambda ()
+	    (jedi:setup)
+	    (jedi:ac-setup)
+            (local-set-key "\C-cd" 'jedi:show-doc)
+            (local-set-key (kbd "M-SPC") 'jedi:complete)
+            (local-set-key (kbd "M-.") 'jedi:goto-definition)))
+
+(setq flymake-python-pyflakes-executable "flake8")
+(add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
+
+(defun flymake-activate ()
+  "Activates flymake when real buffer and you have write access"
+  (if (and
+       (buffer-file-name)
+       (file-writable-p buffer-file-name))
+      (progn
+        (flymake-mode t)
+        ;; this is necessary since there is no flymake-mode-hook...
+        (local-set-key (kbd "C-c n") 'flymake-goto-next-error)
+        (local-set-key (kbd "C-c p") 'flymake-goto-prev-error))))
+
+(defun ca-flymake-show-help ()
+  (when (get-char-property (point) 'flymake-overlay)
+    (let ((help (get-char-property (point) 'help-echo)))
+      (if help (message "%s" help)))))
+
+(add-hook 'post-command-hook 'ca-flymake-show-help)
+
+(add-hook 'python-mode-hook 'flymake-activate)
+(add-hook 'python-mode-hook 'auto-complete-mode)
+
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+;; This is your old M-x.
+;; (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)'
+
+;; -------------------- extra nice things --------------------
+(show-paren-mode t)
+ ; Turn beep off
+(setq visible-bell nil)
+;; Give me path from my bash
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+(setq abbrev-file-name "~/.emacs.d/abbrev_defs")
+
