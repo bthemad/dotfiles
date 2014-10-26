aeecb78421914f7a1fd88b6239a09628a2c2b212
Alex Kudryashov <alex.kudryashov@gmail.com>
2014-10-26 16:06:29 +0100

    Fixing init.sh script so it doesn't complain when files doesn't exist

--- init.sh
+++ init.sh
@@ -1,17 +1,17 @@
 cd ~
-rm ./.bash_profile
-rm ./.bashrc
-rm ./.gitconfig
-rm ./.tmux
-rm ./.zshrc
-rm ./.gdbinit
-rm ./.cgdbrc
+[[ -f ~/.bash_profile ]] && rm ~/.bash_profile
+[[ -f ~/.bashrc ]] && rm ~/.bashrc
+[[ -f ~/.gitconfig ]] && rm ~/.gitconfig
+[[ -f ~/.tmux.conf ]] && rm ~/.tmux.conf
+[[ -f ~/.zshrc ]] && rm ~/.zshrc
+[[ -f ~/.gdbinit ]] && rm ~/.gdbinit
+[[ -f ~/.cgdbrc ]] && rm ~/.cgdbrc
 
-ln -s ~/.dotfiles/bash_profile ./.bash_profile
-ln -s ~/.dotfiles/bashrc ./.bashrc
-ln -s ~/.dotfiles/gitconfig ./.gitconfig
-ln -s ~/.dotfiles/tmux.conf ./.tmux.conf
-ln -s ~/.dotfiles/zshrc ./.zshrc
-ln -s ~/.dotfiles/cgdbrc ./.cgdbrc
-ln -s ~/.dotfiles/rsync_exclude ./.rsync_exclude
+ln -s ~/.dotfiles/bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/bashrc ~/.bashrc
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/zshrc ~/.zshrc
+ln -s ~/.dotfiles/gdbinit ~/.gdbinit
+ln -s ~/.dotfiles/cgdbrc ~/.cgdbrc
 
