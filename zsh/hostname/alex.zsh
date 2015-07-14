814e98d0eac7739d20d9762766b3d6c8f27a201a
Alex Kudryashov <akudryashov@newrelic.com>
2015-07-14 16:35:53 +0200

    Removing host-specific file

--- zsh/hostname/alex.zsh
+++
@@ -1,9 +0,0 @@
-# Environment
-## git config overriding
-export GIT_AUTHOR_EMAIL="akudryashov@newrelic.com"
-export GIT_AUTHOR_NAME="Alex Kudryashov"
-export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
-export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
-
-
-# Path
