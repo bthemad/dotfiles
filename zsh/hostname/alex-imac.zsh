720d996f7433cc8e374ff35a869a9b578c78c040
Alex Kudryashov <alex.kudryashov@gmail.com>
2015-07-14 16:24:58 +0200

    Updating hostname-specific file

--- zsh/hostname/alex-imac.zsh
+++
@@ -1,9 +0,0 @@
-# Environment
-## git config overriding
-export GIT_AUTHOR_EMAIL='akudryashov@newrelic.com'
-export GIT_AUTHOR_NAME='Alex Kudryashov'
-
-
-# Path
-## Path to my bin
-export PATH=/Users/alexander.kudryashov/bin:$PATH
