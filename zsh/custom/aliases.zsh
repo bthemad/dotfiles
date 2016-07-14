c2e5f9a6680d95d95b3541b671b87821795bea26
Alex Kudryashov <akudryashov@newrelic.com>
2016-07-14 12:08:52 +0200

    Making things clean

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -60,7 +60,7 @@ alias ff='find . -type f -name'
 
 # Gradle wrapper
 alias gwc='./gradlew'
-alias gwcc='./gradlew check'
+alias gwcc='./gradlew clean check'
 alias gw='./gradlew -x findbugsMain -x findbugsTest -x checkstyleTest -x checkstyleMain'
 
 alias pcp='pwd | pbcopy'
